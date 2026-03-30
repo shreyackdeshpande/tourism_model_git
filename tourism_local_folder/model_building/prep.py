7df91c7a09897e27b2be27c44c3a18791e5db28e
shreyackdeshpande <shreyackdeshpande11@gmail.com>
2026-03-30 23:06:59 +0530

    Update prep.py

--- tourism_local_folder/model_building/prep.py
+++ tourism_local_folder/model_building/prep.py
@@ -11,7 +11,7 @@ from sklearn.preprocessing import LabelEncoder
 from huggingface_hub import login, HfApi
 
 # Define constants for the dataset and output paths
-api = HfApi(token=os.getenv("TOURISM_HF_TOKEN"))
+api = HfApi(token=os.getenv("HF_TOKEN"))
 DATASET_PATH = "hf://datasets/shreyackdeshpande/tourism/tourism.csv"
 df = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
