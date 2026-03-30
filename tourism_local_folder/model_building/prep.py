36715a4b562f3a43b263b3de1257442e9153edde
shreyackdeshpande <shreyackdeshpande11@gmail.com>
2026-03-30 23:34:39 +0530

    Update prep.py

--- tourism_local_folder/model_building/prep.py
+++ tourism_local_folder/model_building/prep.py
@@ -60,6 +60,6 @@ for file_path in files:
     api.upload_file(
         path_or_fileobj=file_path,
         path_in_repo=file_path.split("/")[-1],  # just the filename
-        repo_id="shreyackdeshpande/tourism-space-HF",
+        repo_id="shreyackdeshpande/tourism",
         repo_type="dataset",
     )
