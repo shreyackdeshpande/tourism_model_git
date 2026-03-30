560ca983887738024afd69b6d7f74a964ec9d5a0
shreyackdeshpande <shreyackdeshpande11@gmail.com>
2026-03-30 23:53:18 +0530

    Update repo_id for uploading to tourism repo

--- tourism_local_folder/hosting/hosting.py
+++ tourism_local_folder/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="tourism_local_folder/deployment",     # the local folder containing your files
-    repo_id = "shreyackdeshpande/tourism-space-HF",          # the target repo
+    repo_id = "shreyackdeshpande/tourism",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
